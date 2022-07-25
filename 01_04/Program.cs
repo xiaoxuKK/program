40e1ebaa04669ed6e8643af45310983592f84213
Administrator <Administrator@L4O1Y2BOM79TAIT>
2022-07-25 23:02:54 +0800

    添加项目文件。

---
+++ 01_04/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string shp;
+            Console.WriteLine("请输入敌人血量：");
+            shp = Console.ReadLine();
+            int hp;
+            hp = int.Parse(shp);
+
+            string satk;
+            Console.WriteLine("请输入玩家的攻击力：");
+            satk = Console.ReadLine();
+            int atk;
+            atk = int.Parse(satk);
+                                 
+
+            Console.WriteLine("按回车攻击敌人");
+            Console.ReadLine();
+
+            hp -= atk;
+            Console.WriteLine("玩家战斗为：{1}，敌人被攻击后的血量是：{0}",hp,atk);
+
+            if (hp < 30)
+            {
+                Console.WriteLine("敌人血量到了斩杀线，按回车斩杀！");
+                Console.ReadLine();
+                Console.WriteLine("斩杀成功，敌人死亡！");
+                
+            }
+            else
+            {
+                Console.WriteLine("继续攻击！");
+                
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 02_01/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 计算BMI,elseif
+            float height = float.Parse(Console.ReadLine());
+            float weight = float.Parse(Console.ReadLine());
+            float BMI = weight / (height * height);
+            Console.WriteLine("Your BMI is {0}",BMI);
+            if(BMI < 18.5f)
+            {
+                Console.WriteLine("guoqing");
+            }
+            else if(BMI < 24f)
+            {
+                Console.WriteLine("zhengchang");
+            }
+            else 
+            {
+                if (BMI < 27f)
+                {
+                    Console.WriteLine("qingfei");
+                }
+                else if (BMI < 30f)
+                {
+                    Console.WriteLine("zhongfei");
+                }
+                else if (BMI < 35f)
+                {
+                    Console.WriteLine("zhongfei");
+                }
+                else
+                {
+                    Console.WriteLine("zhongfei2");
+                }
+            }
+            Console.ReadLine();
+
+            #endregion else
+            #region 计算年龄 if
+
+            //Console.WriteLine("请输入性别");
+            //string sex = Console.ReadLine();
+            //if (sex == "男性")
+            //{
+            //    Console.WriteLine("请输入年龄");
+            //    int age = int.Parse(Console.ReadLine());
+
+            //    if (age < 12)
+            //    {
+            //        Console.WriteLine("正太");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("成男");
+            //    }
+
+            //}
+
+            //else
+            //{
+            //    if (sex == "女性")
+            //    {
+            //        Console.WriteLine("请输入年龄");
+            //        int age = int.Parse(Console.ReadLine());
+
+            //        if (age < 10)
+            //        {
+            //            Console.WriteLine("萝莉");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("成女");
+            //        }
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("性别错位");
+
+            //    }
+
+        }
+        #endregion
+            
+            
+        }
+}
+
